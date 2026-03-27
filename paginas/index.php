570bab1be89d72056801765d3340ee5fe50f5c19
brams <bram.segers@edu-kw1c.nl>
2026-03-27 22:00:51 +0100

    Index gekopieerd van een website die ik heb gemaakt met bootstrap om te testen dat de website responsive is.

---
+++ paginas/index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="../paginas/opmaak.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<header>
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
+        </div>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../afbeeldingen/code.png" alt="Bootstrap code" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="../afbeeldingen/Arduino.png" alt="Arduino" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="../afbeeldingen/aura3.png" alt="reddingsbrigade" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="../afbeeldingen/smallerbram.png" alt="foto" class="d-block w-100">
+            </div>
+        </div>
+
+        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+            <span class="carousel-control-next-icon"></span>
+        </button>
+    </div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="../paginas/bootstrap.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../paginas/C++.html">Embedded</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../paginas/IRW.html">Interactive responsive website</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../paginas/brb.html">Reddingsbrigade</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../paginas/bram.html">Over mij</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
+<main>
+    <h1>
+        Welkom op de website gemaakt door Bram Segers.
+    </h1>
+    <p>
+        Op deze website kun u zien wat ik deze periode heb gedaan voor de modules: Eenvoudig Embedded en Interactive Responsive Website. Verder nog wat informatie over mij en de Reddingsbrigade Den Bosch. Deze website is gemaakt voor de module Interactive Responsive Website om te laten zien wat ik deze periode heb geleerd. En uitleg over wat er deze periode verteld is tijdens de lessen.
+    </p>
+    <p>
+        Kies boven in de navigate balk naar welke pagina u wilt gaan.
+    </p>
+    </div>
+</main>
+<footer>
+</footer>
+<!-- Bootstrap JavaScript Libraries -->
+<script
+    src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+    crossorigin="anonymous"
+></script>
+
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+    integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+    crossorigin="anonymous"
+></script>
+</body>
+</html>
