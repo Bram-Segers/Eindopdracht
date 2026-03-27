d8338ce84d4e85b12170425aec0237b52ea537d8
brams <bram.segers@edu-kw1c.nl>
2026-03-27 23:36:44 +0100

    Zoekveld aangemaakt en gekoppeld aan database alleen het terug krijgen van resultaten werkt niet

--- paginas/index.php
+++ paginas/index.php
@@ -24,36 +24,6 @@
 
 <body>
 <header>
-    <div id="demo" class="carousel slide" data-bs-ride="carousel">
-
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-            <button type="button" data-bs-target="#demo" data-bs-slide-to="3"></button>
-        </div>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="../afbeeldingen/code.png" alt="Bootstrap code" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="../afbeeldingen/Arduino.png" alt="Arduino" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="../afbeeldingen/aura3.png" alt="reddingsbrigade" class="d-block w-100">
-            </div>
-            <div class="carousel-item">
-                <img src="../afbeeldingen/smallerbram.png" alt="foto" class="d-block w-100">
-            </div>
-        </div>
-
-        <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon"></span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-            <span class="carousel-control-next-icon"></span>
-        </button>
-    </div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -80,19 +50,46 @@
             </div>
         </div>
     </nav>
+    <?php
+    include "../includes/db_functions.php";
+
+    StartConnection("studenten_db");
+
+    ?>
 
 </header>
 <main>
-    <h1>
-        Welkom op de website gemaakt door Bram Segers.
-    </h1>
-    <p>
-        Op deze website kun u zien wat ik deze periode heb gedaan voor de modules: Eenvoudig Embedded en Interactive Responsive Website. Verder nog wat informatie over mij en de Reddingsbrigade Den Bosch. Deze website is gemaakt voor de module Interactive Responsive Website om te laten zien wat ik deze periode heb geleerd. En uitleg over wat er deze periode verteld is tijdens de lessen.
-    </p>
-    <p>
-        Kies boven in de navigate balk naar welke pagina u wilt gaan.
-    </p>
+    <form action="index.php" method="get">
+    <div class="input-group flex-nowrap">
+        <span class="input-group-text"</span>
+        <input type="text" class="form-control" placeholder="" aria-label="naamStudent">
+        <input type="submit" value="Zoeken">
     </div>
+    </form>
+
+    <?php
+    if(isset($_GET["studentZoeken"])) {
+        $searchName = $_GET["naamStudent"];
+
+        $query = "SELECT * FROM studenten WHERE Voornaam LIKE '%$searchName%' OR Achternaam LIKE '%$searchName%';";
+
+
+        $resultSearchStudent = ExecuteSelectQuery($query);
+
+
+        foreach ($resultSearchStudent as $row) {
+            $voornaam = $row["Voornaam"];
+
+            echo "<article>";
+            echo $row["Voornaam"];
+            echo "</article>";
+        }
+    }
+
+//    echo $resultSearchStudent;
+
+
+    ?>
 </main>
 <footer>
 </footer>
@@ -100,13 +97,13 @@
 <script
     src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
     integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-    crossorigin="anonymous"
-></script>
+    crossorigin="anonymous">
+</script>
 
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
     integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
-    crossorigin="anonymous"
-></script>
+    crossorigin="anonymous">
+</script>
 </body>
 </html>
